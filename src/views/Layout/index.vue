0a0a4c4c036829f0fa28e35d1bf9ef8c67ffe58d
yc <arigatouyc@foxmail.com>
2023-05-30 09:39:51 +0800

    吸顶dix导航

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,11 +2,20 @@
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutSticky from './components/LayoutSticky.vue'
 </script>
 
 <template>
     <LayoutNav />
+    <LayoutSticky />
     <LayoutHeader />
     <RouterView />
     <LayoutFooter />
+    <div class="s"></div>
 </template>
+
+<style lang="scss">
+.s {
+    height: 2000px;
+}
+</style>
